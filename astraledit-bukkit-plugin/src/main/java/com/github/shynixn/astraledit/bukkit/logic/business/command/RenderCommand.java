441a54f08f9ba20aa81c09cc8fafd2b4399d0f47
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2018-10-02 15:42:50 +0530

    Fix return value

--- astraledit-bukkit-plugin/src/main/java/com/github/shynixn/astraledit/bukkit/logic/business/command/RenderCommand.java
+++ astraledit-bukkit-plugin/src/main/java/com/github/shynixn/astraledit/bukkit/logic/business/command/RenderCommand.java
@@ -33,9 +33,8 @@ public class RenderCommand implements PlayerCommand {
             return false;
         } else {
             createRenderCommand(player);
+            return true;
         }
-
-        return false;
     }
 
     /**
