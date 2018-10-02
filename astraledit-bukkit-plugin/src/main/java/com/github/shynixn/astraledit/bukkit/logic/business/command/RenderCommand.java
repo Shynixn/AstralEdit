0219d84cb52fa86fa48b4b2fd52ed309982fd39f
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2018-10-02 16:17:39 +0530

    Remove the createRenderCommand()
    Also use Plugin instead of JavaPlugin in the constructor.

--- astraledit-bukkit-plugin/src/main/java/com/github/shynixn/astraledit/bukkit/logic/business/command/RenderCommand.java
+++ astraledit-bukkit-plugin/src/main/java/com/github/shynixn/astraledit/bukkit/logic/business/command/RenderCommand.java
@@ -6,17 +6,18 @@ import com.github.shynixn.astraledit.api.bukkit.business.entity.Selection;
 import com.github.shynixn.astraledit.bukkit.AstralEditPlugin;
 import com.github.shynixn.astraledit.bukkit.Permission;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.Plugin;
 
 public class RenderCommand implements PlayerCommand {
 
-    private JavaPlugin plugin;
+    private Plugin plugin;
 
     /**
-     * Creates an instance of RenderCommand which depends on a JavaPlugin
-     * @param plugin the JavaPlugin
+     * Creates an instance of RenderCommand which depends on a Plugin
+     *
+     * @param plugin the Plugin
      */
-    public RenderCommand(JavaPlugin plugin) {
+    public RenderCommand(Plugin plugin) {
         this.plugin = plugin;
     }
 
@@ -32,26 +33,17 @@ public class RenderCommand implements PlayerCommand {
         if (args.length != 1 || !args[0].equalsIgnoreCase("render") || !Permission.RENDER.hasPermission(player)) {
             return false;
         } else {
-            createRenderCommand(player);
+            plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+                player.sendMessage(AstralEditPlugin.PREFIX_SUCCESS + "Rendering WorldEdit-Selection asynchronously...");
+                final Selection selection = AstralEditApi.INSTANCE.render(player);
+                if (selection == null) {
+                    player.sendMessage(AstralEditPlugin.PREFIX_ERROR + "Failed rendering WE selection!");
+                    player.sendMessage(AstralEditPlugin.PREFIX_ERROR + "Check if you selected an area with Worldedit.");
+                } else {
+                    player.sendMessage(AstralEditPlugin.PREFIX_SUCCESS + "Finished rendering selection.");
+                }
+            });
             return true;
         }
-    }
-
-    /**
-     * Creates a new rendered object for the player
-     *
-     * @param player player
-     */
-    private void createRenderCommand(final Player player) {
-        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
-            player.sendMessage(AstralEditPlugin.PREFIX_SUCCESS + "Rendering WorldEdit-Selection asynchronously...");
-            final Selection selection = AstralEditApi.INSTANCE.render(player);
-            if (selection == null) {
-                player.sendMessage(AstralEditPlugin.PREFIX_ERROR + "Failed rendering WE selection!");
-                player.sendMessage(AstralEditPlugin.PREFIX_ERROR + "Check if you selected an area with Worldedit.");
-            } else {
-                player.sendMessage(AstralEditPlugin.PREFIX_SUCCESS + "Finished rendering selection.");
-            }
-        });
     }
 }
